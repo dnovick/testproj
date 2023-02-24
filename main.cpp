df8a89d6d8211bd6b1d841aa57f6d7e9857d1a7f
David Novick <david.o.novick@intel.com>
2023-02-24 07:00:46 -0800

    Playing with CodeChecker setup and github actions.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+//#include <algorithm>
+//#include <numeric>
 
-int main()
+auto func(int b) -> int
 {
-    std::cout << "Hello, Test Project!" << std::endl;
+    int a, c;
 
-    auto v = std::vector<int> {1, 2, 3, 4, 5};
+    switch(b) {
+        case 1:
+            a = b / 0;
+            break;
+        case 4:
+            c = b - 4;
+            a = b/c;
+            break;
 
-    std::cout << "Vector has " << v.size() << " elements.\n";
+    }
+
+    return a;
+}
+
+auto main() -> int
+{
+    std::cout << "Hello, Test Project! Let's try this." << std::endl;
+
+    auto vec = std::vector<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+
+    // auto total = std::reduce(vec.begin(), vec.end());
+
+    // std::cout << "Vector has " << vec.size() << " elements with total: " << total << ".\n";
+
 
 
 
